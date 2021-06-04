cede2be508d02771622418eab60a0a5fe651b3e3
Chris Grahn <grahn@posteo.net>
2021-06-04 14:49:06 -0500

    Delete everything
    This branch is for github pages. So we don't need anything from the
    master branch.

--- dist/src/urls.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.API = process.env.BOTLINK_API_URL || "https://api.botlink.com";
-exports.C3 = process.env.BOTLINK_FLIGHT_URL || "https://c3.botlink.com";
-exports.INFO = process.env.BOTLINK_INFO_URL ||
-    "https://botlink-signal-production.herokuapp.com";
--- src/urls.ts
+++
@@ -1,7 +0,0 @@
-export const API: string =
-  process.env.BOTLINK_API_URL || "https://api.botlink.com";
-export const C3: string =
-  process.env.BOTLINK_FLIGHT_URL || "https://c3.botlink.com";
-export const INFO: string =
-  process.env.BOTLINK_INFO_URL ||
-  "https://botlink-signal-production.herokuapp.com";
