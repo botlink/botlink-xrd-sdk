655e6d7b4281124bc0e0a4d00285f96a204b7312
Mitchell Monaghan <mitchell.monaghan@hotmail.com>
2021-12-17 21:44:18 -0600

    Moved index file to src folder updated package.json to now point at the compiled version of this.

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './src/api';
-export * from './src/xrdSocket';
-export * from './src/xrd';
-export * from './src/auth';
-export * from './src/napi/lib/binding';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from './api'
+export * from './xrdSocket'
+export * from './xrd'
+export * from './auth'
+export * from './napi/lib/binding'
